b9023b7b3255c243539619542ec1af04dcef1598
leviathan-7 <m.grober02@yandex.ru>
2022-09-23 18:03:51 +0300

    MainForm fixed

--- Lab3/RasterAlgorithms/RasterAlgorithms/MainForm.Designer.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RasterAlgorithms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -63,7 +63,7 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -71,7 +71,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Растровые алгоритмы";
             this.ResumeLayout(false);
 
