87da538d9fcd06c825715603f96f9a7ee5de058d
tediorewf <meremeev@sfedu.ru>
2022-09-23 16:49:34 +0300

    Добавлены формы для каждого задания

--- Lab3/RasterAlgorithms/RasterAlgorithms/MainForm.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/MainForm.cs
@@ -19,17 +19,20 @@ namespace RasterAlgorithms
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            var task1Form = new Task1Form();
+            task1Form.ShowDialog();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            var task2Form = new Task2Form();
+            task2Form.ShowDialog();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            var task3Form = new Task3Form();
+            task3Form.ShowDialog();
         }
     }
 }
---
+++ Lab3/RasterAlgorithms/RasterAlgorithms/Task1Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RasterAlgorithms
+{
+    public partial class Task1Form : Form
+    {
+        public Task1Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
