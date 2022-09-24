926a96092636de92209a25b82946fe358f235dfa
tediorewf <meremeev@sfedu.ru>
2022-09-24 20:46:15 +0300

    Добавлена форма для третьего задания

--- Lab3/RasterAlgorithms/RasterAlgorithms/Task3Form.Designer.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/Task3Form.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.addVerticesButton = new System.Windows.Forms.Button();
+            this.performRasterizationButton = new System.Windows.Forms.Button();
+            this.chooseColorButton = new System.Windows.Forms.Button();
+            this.trianglePictureBox = new System.Windows.Forms.PictureBox();
+            this.removeVerticesButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trianglePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addVerticesButton
+            // 
+            this.addVerticesButton.Location = new System.Drawing.Point(12, 12);
+            this.addVerticesButton.Name = "addVerticesButton";
+            this.addVerticesButton.Size = new System.Drawing.Size(220, 80);
+            this.addVerticesButton.TabIndex = 0;
+            this.addVerticesButton.Text = "Добавить вершины";
+            this.addVerticesButton.UseVisualStyleBackColor = true;
+            // 
+            // performRasterizationButton
+            // 
+            this.performRasterizationButton.Location = new System.Drawing.Point(772, 12);
+            this.performRasterizationButton.Name = "performRasterizationButton";
+            this.performRasterizationButton.Size = new System.Drawing.Size(220, 80);
+            this.performRasterizationButton.TabIndex = 1;
+            this.performRasterizationButton.Text = "Выполнить растеризацию";
+            this.performRasterizationButton.UseVisualStyleBackColor = true;
+            // 
+            // chooseColorButton
+            // 
+            this.chooseColorButton.Location = new System.Drawing.Point(267, 12);
+            this.chooseColorButton.Name = "chooseColorButton";
+            this.chooseColorButton.Size = new System.Drawing.Size(220, 80);
+            this.chooseColorButton.TabIndex = 2;
+            this.chooseColorButton.Text = "Выбрать цвет";
+            this.chooseColorButton.UseVisualStyleBackColor = true;
+            // 
+            // trianglePictureBox
+            // 
+            this.trianglePictureBox.Location = new System.Drawing.Point(12, 109);
+            this.trianglePictureBox.Name = "trianglePictureBox";
+            this.trianglePictureBox.Size = new System.Drawing.Size(980, 838);
+            this.trianglePictureBox.TabIndex = 3;
+            this.trianglePictureBox.TabStop = false;
+            this.trianglePictureBox.MouseLeave += new System.EventHandler(this.trianglePictureBox_MouseLeave);
+            this.trianglePictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.trianglePictureBox_MouseMove);
+            // 
+            // removeVerticesButton
+            // 
+            this.removeVerticesButton.Location = new System.Drawing.Point(518, 12);
+            this.removeVerticesButton.Name = "removeVerticesButton";
+            this.removeVerticesButton.Size = new System.Drawing.Size(220, 80);
+            this.removeVerticesButton.TabIndex = 4;
+            this.removeVerticesButton.Text = "Удалить вершины";
+            this.removeVerticesButton.UseVisualStyleBackColor = true;
+            // 
+            // Task3Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Task3Form";
+            this.ClientSize = new System.Drawing.Size(1004, 959);
+            this.Controls.Add(this.removeVerticesButton);
+            this.Controls.Add(this.trianglePictureBox);
+            this.Controls.Add(this.chooseColorButton);
+            this.Controls.Add(this.performRasterizationButton);
+            this.Controls.Add(this.addVerticesButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Task3Form";
+            this.Text = "Растеризация треугольника";
+            ((System.ComponentModel.ISupportInitialize)(this.trianglePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button addVerticesButton;
+        private System.Windows.Forms.Button performRasterizationButton;
+        private System.Windows.Forms.Button chooseColorButton;
+        private System.Windows.Forms.PictureBox trianglePictureBox;
+        private System.Windows.Forms.Button removeVerticesButton;
     }
 }
--- Lab3/RasterAlgorithms/RasterAlgorithms/Task3Form.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/Task3Form.cs
@@ -16,5 +16,15 @@ namespace RasterAlgorithms
         {
             InitializeComponent();
         }
+
+        private void trianglePictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Cross;
+        }
+
+        private void trianglePictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
     }
 }
