4aa9c450d388e361e83b7198961d4daeac86edd0
Restyled.io <commits@restyled.io>
2022-09-24 14:06:25 +0000

    Restyled by astyle

--- Lab3/RasterAlgorithms/RasterAlgorithms/Task1Form.Designer.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/Task1Form.Designer.cs
@@ -1,130 +1,130 @@
 ﻿namespace RasterAlgorithms
 {
-    partial class Task1Form
+partial class Task1Form
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(659, 526);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(768, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 43);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "выбор цвета";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Red;
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(768, 156);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 43);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "кисть";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(768, 229);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 43);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "заливка";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(768, 304);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 43);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "фото-заливка";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(768, 84);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(108, 43);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "выбор фото";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // Task1Form
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1103, 563);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Task1Form";
-            this.Text = "Task1Form";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.pictureBox1 = new System.Windows.Forms.PictureBox();
+        this.button1 = new System.Windows.Forms.Button();
+        this.button2 = new System.Windows.Forms.Button();
+        this.button3 = new System.Windows.Forms.Button();
+        this.button4 = new System.Windows.Forms.Button();
+        this.button5 = new System.Windows.Forms.Button();
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+        this.SuspendLayout();
+        //
+        // pictureBox1
+        //
+        this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+        this.pictureBox1.Name = "pictureBox1";
+        this.pictureBox1.Size = new System.Drawing.Size(659, 526);
+        this.pictureBox1.TabIndex = 0;
+        this.pictureBox1.TabStop = false;
+        this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+        this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+        this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+        //
+        // button1
+        //
+        this.button1.Location = new System.Drawing.Point(768, 12);
+        this.button1.Name = "button1";
+        this.button1.Size = new System.Drawing.Size(108, 43);
+        this.button1.TabIndex = 1;
+        this.button1.Text = "выбор цвета";
+        this.button1.UseVisualStyleBackColor = true;
+        this.button1.Click += new System.EventHandler(this.button1_Click);
+        //
+        // button2
+        //
+        this.button2.BackColor = System.Drawing.Color.Red;
+        this.button2.ForeColor = System.Drawing.Color.Black;
+        this.button2.Location = new System.Drawing.Point(768, 156);
+        this.button2.Name = "button2";
+        this.button2.Size = new System.Drawing.Size(108, 43);
+        this.button2.TabIndex = 2;
+        this.button2.Text = "кисть";
+        this.button2.UseVisualStyleBackColor = false;
+        this.button2.Click += new System.EventHandler(this.button2_Click);
+        //
+        // button3
+        //
+        this.button3.Location = new System.Drawing.Point(768, 229);
+        this.button3.Name = "button3";
+        this.button3.Size = new System.Drawing.Size(108, 43);
+        this.button3.TabIndex = 3;
+        this.button3.Text = "заливка";
+        this.button3.UseVisualStyleBackColor = true;
+        this.button3.Click += new System.EventHandler(this.button3_Click);
+        //
+        // button4
+        //
+        this.button4.Location = new System.Drawing.Point(768, 304);
+        this.button4.Name = "button4";
+        this.button4.Size = new System.Drawing.Size(108, 43);
+        this.button4.TabIndex = 4;
+        this.button4.Text = "фото-заливка";
+        this.button4.UseVisualStyleBackColor = true;
+        this.button4.Click += new System.EventHandler(this.button4_Click);
+        //
+        // button5
+        //
+        this.button5.Location = new System.Drawing.Point(768, 84);
+        this.button5.Name = "button5";
+        this.button5.Size = new System.Drawing.Size(108, 43);
+        this.button5.TabIndex = 5;
+        this.button5.Text = "выбор фото";
+        this.button5.UseVisualStyleBackColor = true;
+        this.button5.Click += new System.EventHandler(this.button5_Click);
+        //
+        // Task1Form
+        //
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(1103, 563);
+        this.Controls.Add(this.button5);
+        this.Controls.Add(this.button4);
+        this.Controls.Add(this.button3);
+        this.Controls.Add(this.button2);
+        this.Controls.Add(this.button1);
+        this.Controls.Add(this.pictureBox1);
+        this.Name = "Task1Form";
+        this.Text = "Task1Form";
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+        this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+}
 }
