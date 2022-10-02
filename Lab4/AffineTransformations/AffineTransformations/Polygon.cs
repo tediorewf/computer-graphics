633f2c7a2012db46be000902e5df6aa1cf6e1d0b
tediorewf <meremeev@sfedu.ru>
2022-10-02 15:15:06 +0300

    Добавлено рисование примитивов

---
+++ Lab4/AffineTransformations/AffineTransformations/Polygon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffineTransformations
+{
+    public class Polygon
+    {
+        public List<Edge> Edges { get; private set; }
+
+        public Polygon(List<Edge> edges)
+        {
+            Edges = edges;
+        }
+
+        // TODO: реализовать методы для полигона. При необходимости реализовать методы для ребра и вызавать в полигоне
+        // (например, для поворота полигона нужно реализовать поворот ребра).
+    }
+}
