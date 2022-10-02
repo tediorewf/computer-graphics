f30af35c2d47e043a951b22e2b97a8e803b15ac6
Restyled.io <commits@restyled.io>
2022-10-02 08:15:57 +0000

    Restyled by clang-format

--- Lab3/RasterAlgorithms/RasterAlgorithms/Task2Form.cs
+++ Lab3/RasterAlgorithms/RasterAlgorithms/Task2Form.cs
@@ -8,63 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RasterAlgorithms
-{
-public partial class Task2Form : Form
-{
-    private List<Point> points;
-    private bool smooth;
+namespace RasterAlgorithms {
+public partial class Task2Form : Form {
+  private List<Point> points;
+  private bool smooth;
 
-    public Task2Form()
-    {
-        InitializeComponent();
+  public Task2Form() {
+    InitializeComponent();
 
-        var size = pictureBox1.Size;
-        pictureBox1.Image = new Bitmap(size.Width, size.Height);
+    var size = pictureBox1.Size;
+    pictureBox1.Image = new Bitmap(size.Width, size.Height);
 
-        points = new List<Point>();
-        smoothCheckBox.Checked = smooth = true;
+    points = new List<Point>();
+    smoothCheckBox.Checked = smooth = true;
+  }
+
+  private void pictureBox1_MouseEnter(object sender, EventArgs e) {
+    Cursor = Cursors.Cross;
+  }
+
+  private void pictureBox1_MouseLeave(object sender, EventArgs e) {
+    Cursor = Cursors.Arrow;
+  }
+
+  private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+    points.Add(e.Location);
+    if (points.Count == 2) {
+      var bmp = pictureBox1.Image as Bitmap;
+      if (smooth) {
+        bmp.DrawWuLine(points[0], points[1]);
+      } else {
+        bmp.DrawBresenhamLine(points[0], points[1], Color.Black);
+      }
+      pictureBox1.Image = bmp;
+      points.Clear();
     }
+  }
 
+  private void smoothCheckBox_CheckedChanged(object sender, EventArgs e) {
+    smooth = smoothCheckBox.Checked;
+  }
 
-    private void pictureBox1_MouseEnter(object sender, EventArgs e)
-    {
-        Cursor = Cursors.Cross;
-    }
-
-    private void pictureBox1_MouseLeave(object sender, EventArgs e)
-    {
-        Cursor = Cursors.Arrow;
-    }
-
-    private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
-    {
-        points.Add(e.Location);
-        if (points.Count == 2)
-        {
-            var bmp = pictureBox1.Image as Bitmap;
-            if (smooth)
-            {
-                bmp.DrawWuLine(points[0], points[1]);
-            }
-            else
-            {
-                bmp.DrawBresenhamLine(points[0], points[1], Color.Black);
-            }
-            pictureBox1.Image = bmp;
-            points.Clear();
-        }
-    }
-
-    private void smoothCheckBox_CheckedChanged(object sender, EventArgs e)
-    {
-        smooth = smoothCheckBox.Checked;
-    }
-
-    private void button1_Click(object sender, EventArgs e)
-    {
-        var size = pictureBox1.Size;
-        pictureBox1.Image = new Bitmap(size.Width, size.Height);
-    }
+  private void button1_Click(object sender, EventArgs e) {
+    var size = pictureBox1.Size;
+    pictureBox1.Image = new Bitmap(size.Width, size.Height);
+  }
 }
 }
