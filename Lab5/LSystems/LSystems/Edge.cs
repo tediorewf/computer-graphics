0210a01b433a7fc3c5415e0d7ec3f8681679bd04
leviathan-7 <m.grober02@yandex.ru>
2022-10-04 15:23:51 +0300

    five lab is LSystems

---
+++ Lab5/LSystems/LSystems/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AffineTransformations
+{
+    public class Edge
+    {
+        public Point Begin { get;  set; }
+        public Point End { get;  set; }
+
+        public Edge(Point begin, Point end)
+        {
+            Begin = begin;
+            End = end;
+        }
+    }
+}
