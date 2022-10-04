018a5a520418e17fac418ee9e8de45a307733561
Restyled.io <commits@restyled.io>
2022-10-04 10:44:02 +0000

    Restyled by astyle

--- Lab4/AffineTransformations/AffineTransformations/Edge.cs
+++ Lab4/AffineTransformations/AffineTransformations/Edge.cs
@@ -7,28 +7,37 @@ using System.Drawing;
 
 namespace AffineTransformations
 {
-    public class Edge
-    {
-        public bool Red { get; set; }
-        public Point Begin { get;  set; }
-        public Point End { get;  set; }
-
-        public Edge(Point begin, Point end)
-        {
-            Begin = begin;
-            End = end;
-        }
-
-        public void Turn90DegreesAroundCenter()
-        {
-            // TODO: реализовать повор на 90 градусов вокруг центра
-        }
-
-        public Point Intersect(Edge other)
-        {
-            var intersectionPoint = new Point();
-            // TODO: реализорвать пересечение ребер
-            return intersectionPoint;
-        }
+public class Edge
+{
+    public bool Red {
+        get;
+        set;
     }
+    public Point Begin {
+        get;
+        set;
+    }
+    public Point End {
+        get;
+        set;
+    }
+
+    public Edge(Point begin, Point end)
+    {
+        Begin = begin;
+        End = end;
+    }
+
+    public void Turn90DegreesAroundCenter()
+    {
+        // TODO: реализовать повор на 90 градусов вокруг центра
+    }
+
+    public Point Intersect(Edge other)
+    {
+        var intersectionPoint = new Point();
+        // TODO: реализорвать пересечение ребер
+        return intersectionPoint;
+    }
+}
 }
--- Lab4/AffineTransformations/AffineTransformations/Matrix.cs
+++ Lab4/AffineTransformations/AffineTransformations/Matrix.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace AffineTransformations
 {
-    class Matrix
-    {
-        private int x;
-        private int y;
-        private double[,] arr;
-        public double X() {
-            return x;
-        }
-        public double Y()
-        {
-            return x;
-        }
-        public Matrix(int x, int y, double[,] arr) {
-            this.x = x;
-            this.y = y;
-            this.arr = arr;
-        }
-        public double[,] Arr()
-        {
-            return arr;
-        }
-        public Matrix Mul(Matrix M) {
-            double[,] A = new double[x, M.y];
-            for (int i = 0; i < x; i++)
-                for (int j = 0; j < M.y; j++) {
-                    double S = 0;
-                    for (int r = 0; r < y; r++)
-                        S += arr[i, r] * M.arr[r, j];
-                    A[i, j] = S;
-                }
-            return new Matrix(x, M.y, A);
-        }
+class Matrix
+{
+    private int x;
+    private int y;
+    private double[,] arr;
+    public double X() {
+        return x;
     }
+    public double Y()
+    {
+        return x;
+    }
+    public Matrix(int x, int y, double[,] arr) {
+        this.x = x;
+        this.y = y;
+        this.arr = arr;
+    }
+    public double[,] Arr()
+    {
+        return arr;
+    }
+    public Matrix Mul(Matrix M) {
+        double[,] A = new double[x, M.y];
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < M.y; j++) {
+                double S = 0;
+                for (int r = 0; r < y; r++)
+                    S += arr[i, r] * M.arr[r, j];
+                A[i, j] = S;
+            }
+        return new Matrix(x, M.y, A);
+    }
+}
 }
