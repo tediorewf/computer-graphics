e284b3fc2a107e34f7d4705b0c5959a3c666c274
Restyled.io <commits@restyled.io>
2022-10-06 17:22:59 +0000

    Restyled by clang-format

--- Lab4/AffineTransformations/AffineTransformations/Edge.cs
+++ Lab4/AffineTransformations/AffineTransformations/Edge.cs
@@ -5,109 +5,87 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace AffineTransformations
-{
-public class Edge
-{
-    public bool Red {
-        get;
-        set;
+namespace AffineTransformations {
+public class Edge {
+  public bool Red { get; set; }
+  public Point Begin { get; set; }
+  public Point End { get; set; }
+  public Point Mid => new Point((Begin.X + End.X) / 2, (Begin.Y + End.Y) / 2);
+  public Point Vector => new Point(End.X - Begin.X, End.Y - Begin.Y);
+  public Point Normal {
+    get {
+      var vector = Vector;
+      var normal = new Point(-vector.Y, vector.X);
+      return normal;
     }
-    public Point Begin {
-        get;
-        set;
-    }
-    public Point End {
-        get;
-        set;
-    }
-    public Point Mid => new Point((Begin.X + End.X) / 2, (Begin.Y + End.Y) / 2);
-    public Point Vector => new Point(End.X - Begin.X, End.Y - Begin.Y);
-    public Point Normal
-    {
-        get
-        {
-            var vector = Vector;
-            var normal = new Point(-vector.Y, vector.X);
-            return normal;
-        }
-    }
+  }
 
-    public Edge(Point begin, Point end)
-    {
-        Begin = begin;
-        End = end;
+  public Edge(Point begin, Point end) {
+    Begin = begin;
+    End = end;
+  }
+
+  public void Rotate() {
+    var mid = Mid;
+    var normal = Normal;
+    Begin = new Point(mid.X + normal.X / 2, mid.Y + normal.Y / 2);
+    End = new Point(mid.X - normal.X / 2, mid.Y - normal.Y / 2);
+  }
+
+  public static bool DoesIntersect(Point a, Point b, Point c, Point d) {
+    try {
+      var _intersectionPoint = Intersect(a, b, c, d);
+      return true;
+    } catch (EdgesDontIntersectException) {
+      return false;
     }
+  }
 
-    public void Rotate()
-    {
-        var mid = Mid;
-        var normal = Normal;
-        Begin = new Point(mid.X + normal.X / 2, mid.Y + normal.Y / 2);
-        End = new Point(mid.X - normal.X / 2, mid.Y - normal.Y / 2);
+  public Point Intersect(Edge other) {
+    var a = Begin;
+    var b = End;
+
+    var c = other.Begin;
+    var d = other.End;
+
+    return Intersect(a, b, c, d);
+  }
+
+  public static Point Intersect(Point a, Point b, Point c, Point d) {
+    var n = new Point(d.Y - c.Y, c.X - d.X);
+
+    int denominator = ScalarProduct(n, new Edge(b, a).Vector);
+    if (denominator == 0) {
+      throw new EdgesDontIntersectException();
     }
+    int numerator = -ScalarProduct(n, new Edge(a, c).Vector);
 
-    public static bool DoesIntersect(Point a, Point b, Point c, Point d)
-    {
-        try
-        {
-            var _intersectionPoint = Intersect(a, b, c, d);
-            return true;
-        }
-        catch (EdgesDontIntersectException)
-        {
-            return false;
-        }
-    }
+    double t = (double)numerator / denominator;
 
-    public Point Intersect(Edge other)
-    {
-        var a = Begin;
-        var b = End;
+    var p = ParametrizePoint(a, b, t);
 
-        var c = other.Begin;
-        var d = other.End;
-
-        return Intersect(a, b, c, d);
+    if (!IsPointOnEdge(new Edge(a, b), p) ||
+        !IsPointOnEdge(new Edge(c, d), p)) {
+      throw new EdgesDontIntersectException();
     }
 
-    public static Point Intersect(Point a, Point b, Point c, Point d)
-    {
-        var n = new Point(d.Y - c.Y, c.X - d.X);
+    return p;
+  }
 
-        int denominator = ScalarProduct(n, new Edge(b, a).Vector);
-        if (denominator == 0)
-        {
-            throw new EdgesDontIntersectException();
-        }
-        int numerator = -ScalarProduct(n, new Edge(a, c).Vector);
+  private static bool IsPointOnEdge(Edge edge, Point p) {
+    return Math.Min(edge.Begin.X, edge.End.X) <= p.X &&
+           p.X <= Math.Max(edge.Begin.X, edge.End.X) &&
+           Math.Min(edge.Begin.Y, edge.End.Y) <= p.Y &&
+           p.Y <= Math.Max(edge.Begin.Y, edge.End.Y);
+  }
 
-        double t = (double)numerator / denominator;
+  private static Point ParametrizePoint(Point begin, Point end, double t) {
+    return new Point(begin.X + (int)(t * (end.X - begin.X)),
+                     begin.Y + (int)(t * (end.Y - begin.Y)));
+  }
 
-        var p = ParametrizePoint(a, b, t);
-
-        if (!IsPointOnEdge(new Edge(a, b), p) || !IsPointOnEdge(new Edge(c, d), p))
-        {
-            throw new EdgesDontIntersectException();
-        }
-
-        return p;
-    }
-
-    private static bool IsPointOnEdge(Edge edge, Point p)
-    {
-        return Math.Min(edge.Begin.X, edge.End.X) <= p.X && p.X <= Math.Max(
-                   edge.Begin.X, edge.End.X) && Math.Min(edge.Begin.Y, edge.End.Y) <= p.Y && p.Y <= Math.Max(edge.Begin.Y, edge.End.Y);
-    }
-
-    private static Point ParametrizePoint(Point begin, Point end, double t)
-    {
-        return new Point(begin.X + (int)(t * (end.X - begin.X)), begin.Y + (int)(t * (end.Y - begin.Y)));
-    }
-
-    private static int ScalarProduct(Point vec1, Point vec2)
-    {
-        return vec1.X * vec2.X + vec1.Y * vec2.Y;
-    }
+  private static int ScalarProduct(Point vec1, Point vec2) {
+    return vec1.X * vec2.X + vec1.Y * vec2.Y;
+  }
 }
 }
