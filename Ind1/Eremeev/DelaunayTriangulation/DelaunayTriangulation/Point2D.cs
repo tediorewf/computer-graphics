dd73fd782c407ba1b7f9d930256f0943ff68d7dd
Restyled.io <commits@restyled.io>
2022-11-05 17:41:52 +0000

    Restyled by clang-format

--- Ind1/Eremeev/DelaunayTriangulation/DelaunayTriangulation/Point2D.cs
+++ Ind1/Eremeev/DelaunayTriangulation/DelaunayTriangulation/Point2D.cs
@@ -1,58 +1,41 @@
 ﻿using System.Drawing;
 using System.Collections.Generic;
 
-namespace DelaunayTriangulation
-{
-public class Point2D
-{
-    public int X {
-        get;
-        private set;
-    }
-    public int Y {
-        get;
-        private set;
-    }
-    public HashSet<Triangle2D> AdjacentTriangles {
-        get;
-        private set;
-    }
+namespace DelaunayTriangulation {
+public class Point2D {
+  public int X { get; private set; }
+  public int Y { get; private set; }
+  public HashSet<Triangle2D> AdjacentTriangles { get; private set; }
 
-    public Point2D(int x, int y)
-    {
-        X = x;
-        Y = y;
-        AdjacentTriangles = new HashSet<Triangle2D>();
+  public Point2D(int x, int y) {
+    X = x;
+    Y = y;
+    AdjacentTriangles = new HashSet<Triangle2D>();
+  }
+
+  public Point2D(Point p) : this(p.X, p.Y) {}
+
+  public Point ToPoint() => new Point(X, Y);
+
+  public override bool Equals(object obj) {
+    if (obj == null || obj.GetType() != GetType()) {
+      return false;
     }
 
-    public Point2D(Point p) : this(p.X, p.Y)
-    {
-    }
+    var point = obj as Point2D;
 
-    public Point ToPoint() => new Point(X, Y);
+    return point.X == X && point.Y == Y;
+  }
 
-    public override bool Equals(object obj)
-    {
-        if (obj == null || obj.GetType() != GetType())
-        {
-            return false;
-        }
+  public override int GetHashCode() => X ^ Y;
 
-        var point = obj as Point2D;
+  public int
+  ComputeDistanceSquared(Point2D other) => ComputeDistanceSquared(this, other);
 
-        return point.X == X && point.Y == Y;
-    }
-
-    public override int GetHashCode() => X ^ Y;
-
-    public int ComputeDistanceSquared(Point2D other)
-    => ComputeDistanceSquared(this, other);
-
-    public static int ComputeDistanceSquared(Point2D lhs, Point2D rhs)
-    {
-        int diffX = lhs.X - rhs.X;
-        int diffY = lhs.Y - rhs.Y;
-        return diffX * diffX + diffY * diffY;
-    }
+  public static int ComputeDistanceSquared(Point2D lhs, Point2D rhs) {
+    int diffX = lhs.X - rhs.X;
+    int diffY = lhs.Y - rhs.Y;
+    return diffX * diffX + diffY * diffY;
+  }
 }
 }
