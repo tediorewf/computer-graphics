a7334790ac04413106056552fc731c8bdec46a01
Restyled.io <commits@restyled.io>
2022-11-25 10:01:08 +0000

    Restyled by clang-format

--- Lab6/AffineTransformations3D/AffineTransformations3D/FacetRemovingType.cs
+++ Lab6/AffineTransformations3D/AffineTransformations3D/FacetRemovingType.cs
@@ -4,30 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AffineTransformations3D
-{
-public enum FacetRemovingType
-{
-    None, ZBuffer, ZBufferWithTexturing, BackfaceCulling
+namespace AffineTransformations3D {
+public enum FacetRemovingType {
+  None,
+  ZBuffer,
+  ZBufferWithTexturing,
+  BackfaceCulling
 }
 
-public static class FacetRemovingTypeExtensions
-{
-    public static string GetFacetRemovingName(this FacetRemovingType facetRemovingType)
-    {
-        switch (facetRemovingType)
-        {
-        case FacetRemovingType.None:
-            return "Без отсечения (рисование ребер)";
-        case FacetRemovingType.ZBuffer:
-            return "Z-буфер";
-        case FacetRemovingType.ZBufferWithTexturing:
-            return "Z-буфер с текстурированием";
-        case FacetRemovingType.BackfaceCulling:
-            return "Отчесение по нормалям поверхностей";
-        default:
-            throw new ArgumentException("Неизвестный тип отсечения граней");
-        }
+public static class FacetRemovingTypeExtensions {
+  public static string
+  GetFacetRemovingName(this FacetRemovingType facetRemovingType) {
+    switch (facetRemovingType) {
+    case FacetRemovingType.None:
+      return "Без отсечения (рисование ребер)";
+    case FacetRemovingType.ZBuffer:
+      return "Z-буфер";
+    case FacetRemovingType.ZBufferWithTexturing:
+      return "Z-буфер с текстурированием";
+    case FacetRemovingType.BackfaceCulling:
+      return "Отчесение по нормалям поверхностей";
+    default:
+      throw new ArgumentException("Неизвестный тип отсечения граней");
     }
+  }
 }
 }
