549f269f2a675e151506edc8c3aab3e3e275be26
tediorewf <meremeev@sfedu.ru>
2022-12-07 22:33:56 +0300

    Начата лаба 13
    Создан проект для 13-й лабы, добавлены модельки и текстуры к ним

---
+++ Lab13/Lab13/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <GL/glew.h>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+GLuint Program;
+GLint Attrib_vertex;
+GLint Attrib_color;
+GLuint VBO;
+
+auto main() -> int
+{
+
+}
