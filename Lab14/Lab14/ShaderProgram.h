681e6278a7ba46679c5b27d2404f19511cef6258
tediorewf <meremeev@sfedu.ru>
2022-12-11 11:26:36 +0300

    Выделил шейдерную программу в отдельный класс

--- Lab14/Lab14/ShaderProgram.h
+++ Lab14/Lab14/ShaderProgram.h
@@ -1,7 +1,10 @@
 #ifndef SHADER_PROHRAM_H
 #define SHADER_PROGRAM_H
 
+#include "utils.h"
+
 #include <string>
+#include <iostream>
 
 #include <gl/glew.h>
 
@@ -9,16 +12,68 @@ class ShaderProgram
 {
 	const char* vertexShaderSource;
 	const char* fragmentShaderSource;
-	GLuint programId;
-public:
-	ShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource) 
-		: vertexShaderSource(vertexShaderSource), fragmentShaderSource(fragmentShaderSource)
+	GLuint Program;
+	GLuint vShader, fShader;
+	std::vector<const char*> shaderAttributeNames;
+
+	void initShaders()
 	{
+		vShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vShader);
+		std::cout << "vertex shader \n";
+		ShaderLog(vShader);
+
+		fShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fShader);
+		std::cout << "fragment shader \n";
+		ShaderLog(fShader);
+
+		Program = glCreateProgram();
+
+		glAttachShader(Program, vShader);
+		glAttachShader(Program, fShader);
+
+		glLinkProgram(Program);
+		int link_ok;
+		glGetProgramiv(Program, GL_LINK_STATUS, &link_ok);
+		if (!link_ok)
+		{
+			std::cout << "error attach shaders \n";
+		}
+
+		for (auto shaderAttributeName : shaderAttributeNames)
+		{
+			GLuint shaderAttribute = glGetAttribLocation(Program, shaderAttributeName);
+			if (shaderAttribute == -1)
+			{
+				std::cout << "could not bind attrib " << shaderAttributeName << std::endl;
+			}
+		}
+
+		checkOpenGLerror();
+	}
+public:
+	ShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource, const std::vector<const char*>& shaderAttributeNames)
+		: vertexShaderSource(vertexShaderSource), fragmentShaderSource(fragmentShaderSource), shaderAttributeNames(shaderAttributeNames)
+	{
+		initShaders();
 	}
 
-	static ShaderProgram CreateFromGLSLSourceCodeFiles(const std::string& vertexShaderSourceFilename, const std::string fragmentShaderSourceFilename)
+	GLuint GetID()
 	{
+		return this->Program;
+	}
 
+	void Use()
+	{
+		glUseProgram(Program);
+	}
+
+	void Delete()
+	{
+		glDeleteProgram(Program);
 	}
 };
 
