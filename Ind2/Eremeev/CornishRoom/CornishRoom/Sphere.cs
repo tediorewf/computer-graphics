823e1bbc10eac60f6a07a516a47438b24db7b0f0
tediorewf <meremeev@sfedu.ru>
2022-12-12 10:11:02 +0300

    Продолжаю рейтрэйс

---
+++ Ind2/Eremeev/CornishRoom/CornishRoom/Sphere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornishRoom
+{
+    public class Sphere: Primitive
+    {
+        public Point3D Center { get; set; }
+        public double Radius { get; set; }
+
+        public override bool DefineIfIntersect(Ray straighLine)
+        {
+            return true;
+        }
+    }
+}
