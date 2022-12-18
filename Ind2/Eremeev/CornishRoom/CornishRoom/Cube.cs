d26f217ef5842056ed11f6f896c4391a9e4b59e2
Restyled.io <commits@restyled.io>
2022-12-18 17:46:04 +0000

    Restyled by astyle

--- Ind2/Eremeev/CornishRoom/CornishRoom/Cube.cs
+++ Ind2/Eremeev/CornishRoom/CornishRoom/Cube.cs
@@ -6,30 +6,32 @@ using System.Threading.Tasks;
 
 namespace CornishRoom
 {
-    public class Cube : Primitive
-    {
-        private Vector3D position;
-        private double edgeLength;
-        private double epsilon;
+public class Cube : Primitive
+{
+    private Vector3D position;
+    private double edgeLength;
+    private double epsilon;
 
-        public Vector3D[] Vectices { get; }
-
-        public Cube(Vector3D position, double edgeLength, Material material, double epsilon = 0.001) 
-            : base(material)
-        {
-            this.position = position;
-            this.edgeLength = edgeLength;
-            this.epsilon = epsilon;
-        }
-
-        public override Tuple<double, double> Intersect(Vector3D origin, Vector3D direction)
-        {
-            return Tuple.Create(double.MaxValue, double.MaxValue);
-        }
-
-        public override Vector3D ComputeNormal(Vector3D point)
-        {
-            throw new NotImplementedException();
-        }
+    public Vector3D[] Vectices {
+        get;
     }
+
+    public Cube(Vector3D position, double edgeLength, Material material, double epsilon = 0.001)
+        : base(material)
+    {
+        this.position = position;
+        this.edgeLength = edgeLength;
+        this.epsilon = epsilon;
+    }
+
+    public override Tuple<double, double> Intersect(Vector3D origin, Vector3D direction)
+    {
+        return Tuple.Create(double.MaxValue, double.MaxValue);
+    }
+
+    public override Vector3D ComputeNormal(Vector3D point)
+    {
+        throw new NotImplementedException();
+    }
+}
 }
--- Ind2/Eremeev/CornishRoom/CornishRoom/Plane.cs
+++ Ind2/Eremeev/CornishRoom/CornishRoom/Plane.cs
@@ -2,41 +2,45 @@
 
 namespace CornishRoom
 {
-    public class Plane : Primitive
+public class Plane : Primitive
+{
+    private double epsilon;
+
+    public Vector3D Position {
+        get;
+    }
+    public Vector3D Normal {
+        get;
+    }
+
+    public Plane(Vector3D position, Vector3D normal, Material material, double epsilon = 0.001)
+        : base(material)
     {
-        private double epsilon;
+        this.epsilon = epsilon;
 
-        public Vector3D Position { get; }
-        public Vector3D Normal { get; }
+        Position = position;
+        Normal = normal;
+    }
 
-        public Plane(Vector3D position, Vector3D normal, Material material, double epsilon = 0.001) 
-            : base(material)
+    // https://www.delftstack.com/howto/cpp/intersection-of-ray-and-plane-in-cpp/
+    public override Tuple<double, double> Intersect(Vector3D origin, Vector3D direction)
+    {
+        double denom = Normal.ComputeDotProduct(direction);
+        if (Math.Abs(denom) < epsilon)
         {
-            this.epsilon = epsilon;
-
-            Position = position;
-            Normal = normal;
-        }
-
-        // https://www.delftstack.com/howto/cpp/intersection-of-ray-and-plane-in-cpp/
-        public override Tuple<double, double> Intersect(Vector3D origin, Vector3D direction)
-        {
-            double denom = Normal.ComputeDotProduct(direction);
-            if (Math.Abs(denom) < epsilon)
-            {
-                return Tuple.Create(double.MaxValue, double.MaxValue);
-            }
-
-            var ab = Position - origin;
-            double t = ab.ComputeDotProduct(Normal) / denom;
-            if (Math.Abs(t) >= epsilon)
-            {
-                return Tuple.Create(t, t);
-            }
-
             return Tuple.Create(double.MaxValue, double.MaxValue);
         }
 
-        public override Vector3D ComputeNormal(Vector3D point) => Normal;
+        var ab = Position - origin;
+        double t = ab.ComputeDotProduct(Normal) / denom;
+        if (Math.Abs(t) >= epsilon)
+        {
+            return Tuple.Create(t, t);
+        }
+
+        return Tuple.Create(double.MaxValue, double.MaxValue);
     }
+
+    public override Vector3D ComputeNormal(Vector3D point) => Normal;
+}
 }
