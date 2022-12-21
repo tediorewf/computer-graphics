08f3b1d876d3a468bd38efc33bec0bb02d8091d3
Restyled.io <commits@restyled.io>
2022-12-21 12:45:57 +0000

    Restyled by astyle

--- Lab14/Lab14/Camera.h
+++ Lab14/Lab14/Camera.h
@@ -10,90 +10,90 @@
 
 class Camera
 {
-	glm::vec3 position, up, front;
-	GLfloat pitch, yaw, speed, sensitivity;
+    glm::vec3 position, up, front;
+    GLfloat pitch, yaw, speed, sensitivity;
 
-	bool wasNotMovedBefore;
-	GLfloat previousX, previousY;
+    bool wasNotMovedBefore;
+    GLfloat previousX, previousY;
 
-	void setFront()
-	{
-		GLfloat cosPitch = cos(glm::radians(pitch));
-		auto cameraFront = 
-		front = glm::normalize(
-			glm::vec3(
-				cos(glm::radians(yaw)) * cosPitch, 
-			    sin(glm::radians(pitch)), 
-			    sin(glm::radians(yaw)) * cos(glm::radians(pitch))
-			)
-		);
-	}
+    void setFront()
+    {
+        GLfloat cosPitch = cos(glm::radians(pitch));
+        auto cameraFront =
+            front = glm::normalize(
+                        glm::vec3(
+                            cos(glm::radians(yaw)) * cosPitch,
+                            sin(glm::radians(pitch)),
+                            sin(glm::radians(yaw)) * cos(glm::radians(pitch))
+                        )
+                    );
+    }
 public:
-	Camera(glm::vec3 position, GLfloat pitch, GLfloat yaw, GLfloat speed = 5.0f, GLfloat sensitivity = 1.0f)
-		: position(position), up(glm::vec3(0.0f, 0.0f, 1.0f)), pitch(pitch), yaw(yaw), speed(speed), sensitivity(sensitivity), wasNotMovedBefore(true)
-	{
-		setFront();
-	}
+    Camera(glm::vec3 position, GLfloat pitch, GLfloat yaw, GLfloat speed = 5.0f, GLfloat sensitivity = 1.0f)
+        : position(position), up(glm::vec3(0.0f, 0.0f, 1.0f)), pitch(pitch), yaw(yaw), speed(speed), sensitivity(sensitivity), wasNotMovedBefore(true)
+    {
+        setFront();
+    }
 
-	glm::vec3 getPosition() const
-	{
-		return position;
-	}
+    glm::vec3 getPosition() const
+    {
+        return position;
+    }
 
-	glm::vec3 getFront() const
-	{
-		return front;
-	}
+    glm::vec3 getFront() const
+    {
+        return front;
+    }
 
-	void moveForward() 
-	{
-		position += front * speed;
-	}
+    void moveForward()
+    {
+        position += front * speed;
+    }
 
-	void moveBackward()
-	{
-		position -= front * speed;
-	}
+    void moveBackward()
+    {
+        position -= front * speed;
+    }
 
-	void moveRight()
-	{
-		auto right = glm::normalize(glm::cross(front, up));
-		position += right * speed;
-	}
+    void moveRight()
+    {
+        auto right = glm::normalize(glm::cross(front, up));
+        position += right * speed;
+    }
 
-	void moveLeft()
-	{
-		auto right = glm::normalize(glm::cross(front, up));
-		position -= right * speed;
-	}
+    void moveLeft()
+    {
+        auto right = glm::normalize(glm::cross(front, up));
+        position -= right * speed;
+    }
 
-	void rotateMouse(GLfloat x, GLfloat y)
-	{
-		if (wasNotMovedBefore)
-		{
-			previousX = x;
-			previousY = y;
-			wasNotMovedBefore = false;
-		}
+    void rotateMouse(GLfloat x, GLfloat y)
+    {
+        if (wasNotMovedBefore)
+        {
+            previousX = x;
+            previousY = y;
+            wasNotMovedBefore = false;
+        }
 
-		GLfloat xOffset = previousX - x;
-		GLfloat yOffset = previousY - y;
-		previousX = x;
-		previousY = y;
+        GLfloat xOffset = previousX - x;
+        GLfloat yOffset = previousY - y;
+        previousX = x;
+        previousY = y;
 
-		xOffset *= sensitivity;
-		yOffset *= sensitivity;
+        xOffset *= sensitivity;
+        yOffset *= sensitivity;
 
-		yaw += yOffset;
-		pitch += xOffset;
+        yaw += yOffset;
+        pitch += xOffset;
 
-		setFront();
-	}
+        setFront();
+    }
 
-	glm::mat4 getViewMatrix() const 
-	{
-		return glm::lookAt(position, position + front, up);
-	}
+    glm::mat4 getViewMatrix() const
+    {
+        return glm::lookAt(position, position + front, up);
+    }
 };
 
 #endif // !CAMERA_H
