0342ee7cdf6912a481bbc3065489c98cfa1bbb20
Restyled.io <commits@restyled.io>
2022-12-28 16:59:11 +0000

    Restyled by astyle

--- Ind3/Eremeev/TankWorld/TankWorld/Camera.h
+++ Ind3/Eremeev/TankWorld/TankWorld/Camera.h
@@ -12,90 +12,90 @@
 
 class Camera
 {
-	glm::vec3 position, up, front, worldUp, right;
-	GLfloat pitch, distanceToPlayer, angleAroundPlayer;
-	Player player;
+    glm::vec3 position, up, front, worldUp, right;
+    GLfloat pitch, distanceToPlayer, angleAroundPlayer;
+    Player player;
 public:
-	Camera(const Player &player, GLfloat pitch, GLfloat distanceToPlayer, glm::vec3 worldUp, GLfloat angleAroundPlayer)
-		: player(player), pitch(pitch), up(worldUp), worldUp(worldUp), distanceToPlayer(distanceToPlayer), angleAroundPlayer(angleAroundPlayer)
-	{
-		updateVectors();
-	}
+    Camera(const Player &player, GLfloat pitch, GLfloat distanceToPlayer, glm::vec3 worldUp, GLfloat angleAroundPlayer)
+        : player(player), pitch(pitch), up(worldUp), worldUp(worldUp), distanceToPlayer(distanceToPlayer), angleAroundPlayer(angleAroundPlayer)
+    {
+        updateVectors();
+    }
 
-	void setPitch(GLfloat pitch)
-	{
-		this->pitch = pitch;
-	}
+    void setPitch(GLfloat pitch)
+    {
+        this->pitch = pitch;
+    }
 
-	void setDistanceToPlayer(GLfloat distanceToPlayer)
-	{
-		this->distanceToPlayer = distanceToPlayer;
-	}
+    void setDistanceToPlayer(GLfloat distanceToPlayer)
+    {
+        this->distanceToPlayer = distanceToPlayer;
+    }
 
-	void setAngleAroundPlayer(GLfloat angleAroundPlayer)
-	{
-		this->angleAroundPlayer = angleAroundPlayer;
-	}
+    void setAngleAroundPlayer(GLfloat angleAroundPlayer)
+    {
+        this->angleAroundPlayer = angleAroundPlayer;
+    }
 
-	const Player& getPlayer() const
-	{
-		return player;
-	}
+    const Player& getPlayer() const
+    {
+        return player;
+    }
 
-	glm::vec3 getPosition() const
-	{
-		return position;
-	}
+    glm::vec3 getPosition() const
+    {
+        return position;
+    }
 
-	glm::vec3 getFront() const
-	{
-		return front;
-	}
+    glm::vec3 getFront() const
+    {
+        return front;
+    }
 
-	void move(const sf::Event& e, GLfloat framesRenderTimeDelta)
-	{
-		player.move(e, framesRenderTimeDelta);
-		updateVectors();
-	}
+    void move(const sf::Event& e, GLfloat framesRenderTimeDelta)
+    {
+        player.move(e, framesRenderTimeDelta);
+        updateVectors();
+    }
 
-	void updateVectors()
-	{
-		GLfloat radiansPitch = glm::radians(pitch);
-		GLfloat sinPitch = static_cast<GLfloat>(sin(radiansPitch));
-		GLfloat cosPitch = static_cast<GLfloat>(cos(radiansPitch));
+    void updateVectors()
+    {
+        GLfloat radiansPitch = glm::radians(pitch);
+        GLfloat sinPitch = static_cast<GLfloat>(sin(radiansPitch));
+        GLfloat cosPitch = static_cast<GLfloat>(cos(radiansPitch));
 
-		GLfloat horizontalDistance = distanceToPlayer * cosPitch;
-		GLfloat verticalDistance = distanceToPlayer * sinPitch;
+        GLfloat horizontalDistance = distanceToPlayer * cosPitch;
+        GLfloat verticalDistance = distanceToPlayer * sinPitch;
 
-		GLfloat playerRotation = player.getRotationY();
-		GLfloat radiansPlayerRotation = glm::radians(playerRotation);
-		GLfloat radiansAngleAroundPlayer = glm::radians(angleAroundPlayer);
-		GLfloat offsetX = horizontalDistance * static_cast<GLfloat>(sin(radiansPlayerRotation + radiansAngleAroundPlayer));
-		GLfloat offsetZ = horizontalDistance * static_cast<GLfloat>(cos(radiansPlayerRotation + radiansAngleAroundPlayer));
+        GLfloat playerRotation = player.getRotationY();
+        GLfloat radiansPlayerRotation = glm::radians(playerRotation);
+        GLfloat radiansAngleAroundPlayer = glm::radians(angleAroundPlayer);
+        GLfloat offsetX = horizontalDistance * static_cast<GLfloat>(sin(radiansPlayerRotation + radiansAngleAroundPlayer));
+        GLfloat offsetZ = horizontalDistance * static_cast<GLfloat>(cos(radiansPlayerRotation + radiansAngleAroundPlayer));
 
-		auto playerPosition = player.getPosition();
+        auto playerPosition = player.getPosition();
 
-		position.x = playerPosition.x - offsetX;
-		position.y = playerPosition.y + verticalDistance;
-		position.z = playerPosition.z - offsetZ;
+        position.x = playerPosition.x - offsetX;
+        position.y = playerPosition.y + verticalDistance;
+        position.z = playerPosition.z - offsetZ;
 
-		GLfloat yaw = playerRotation + angleAroundPlayer;
-		GLfloat radiansYaw = glm::radians(yaw);
-		front = glm::normalize(
-			glm::vec3(
-				static_cast<GLfloat>(sin(radiansYaw)) * cosPitch,
-				sinPitch,
-				static_cast<GLfloat>(cos(radiansYaw)) * cosPitch
-			)
-		);
-		right = glm::normalize(glm::cross(front, worldUp));
-		up = glm::normalize(glm::cross(right, front));
-	}
+        GLfloat yaw = playerRotation + angleAroundPlayer;
+        GLfloat radiansYaw = glm::radians(yaw);
+        front = glm::normalize(
+                    glm::vec3(
+                        static_cast<GLfloat>(sin(radiansYaw)) * cosPitch,
+                        sinPitch,
+                        static_cast<GLfloat>(cos(radiansYaw)) * cosPitch
+                    )
+                );
+        right = glm::normalize(glm::cross(front, worldUp));
+        up = glm::normalize(glm::cross(right, front));
+    }
 
-	glm::mat4 getViewMatrix() const 
-	{
-		return glm::lookAt(position, position + front, up);
-	}
+    glm::mat4 getViewMatrix() const
+    {
+        return glm::lookAt(position, position + front, up);
+    }
 };
 
 #endif // !CAMERA_H
